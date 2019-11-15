d3ec268dc6b74815ce20a757c91c1816cb87ff6d
Jesse Zhuang <zhzexi@gmail.com>
2019-11-14 20:57:45 -0800

    base64 continue

--- src/test/java/github/incodelearning/security/Base64Test.java
+++ src/test/java/github/incodelearning/security/Base64Test.java
@@ -1,16 +1,11 @@
 package github.incodelearning.security;
 
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.Streams;
 import org.junit.Test;
 
-import java.util.Arrays;
 import java.util.Base64;
-import java.util.stream.Stream;
-import java.util.stream.StreamSupport;
 
 import static github.incodelearning.basics.Primitives.byteArrayToBinaryStrings;
-import static github.incodelearning.basics.Primitives.byteToBinaryString;
 import static org.junit.Assert.assertEquals;
 
 public class Base64Test {
@@ -72,4 +67,47 @@ public class Base64Test {
         assertEquals('g', 0b01100111);
         assertEquals(ImmutableList.of("01011010", "01100111"), byteArrayToBinaryStrings(unPaddedEncoded.getBytes()));
     }
+
+    /**
+     * It uses the URL and Filename safe Base64 alphabet and does not add any line separation.
+     */
+    @Test
+    public void testEncodeUrl() {
+        String originalUrl = "https://www.google.co.nz/?gfe_rd=cr&ei=dzbFV&gws_rd=ssl#q=java";
+        String encodedUrl = Base64.getUrlEncoder().encodeToString(originalUrl.getBytes());
+        assertEquals("aHR0cHM6Ly93d3cuZ29vZ2xlLmNvLm56Lz9nZmVfcmQ9Y3ImZWk9ZHpiRlYmZ3dzX3JkPXNzbCNxPWphdmE=",
+                encodedUrl);
+        assertEquals(originalUrl, new String(Base64.getUrlDecoder().decode(encodedUrl)));
+    }
+
+    /**
+     * The MIME encoder generates a Base64 encoded output using the basic alphabet but in a MIME friendly format:
+     * each line of the output is no longer than 76 characters and ends with a carriage return
+     * followed by a linefeed (\r\n).
+     */
+    @Test
+    public void testMIME() {
+        byte[] encodedAsBytes = ("Long test string more than 76 characters without any line break." +
+                "Another line to fill more than 76 characters and test.").getBytes();
+        String encodedMime = Base64.getMimeEncoder().encodeToString(encodedAsBytes);
+        String expected = "TG9uZyB0ZXN0IHN0cmluZyBtb3JlIHRoYW4gNzYgY2hhcmFjdGVycyB3aXRob3V0IGFueSBsaW5l\r\n" +
+                "IGJyZWFrLkFub3RoZXIgbGluZSB0byBmaWxsIG1vcmUgdGhhbiA3NiBjaGFyYWN0ZXJzIGFuZCB0\r\n" +
+                "ZXN0Lg==";
+        assertEquals(expected, encodedMime);
+    }
+
+    @Test
+    public void testApache() {
+        String originalInput = "test input";
+        String expected = "dGVzdCBpbnB1dA==";
+        org.apache.commons.codec.binary.Base64 base64 = new org.apache.commons.codec.binary.Base64();
+        String encodedString = new String(base64.encode(originalInput.getBytes()));
+        assertEquals(expected, encodedString);
+        // alternatively static method
+        String encodedString2 = new String(org.apache.commons.codec.binary.Base64.encodeBase64(originalInput.getBytes()));
+        assertEquals(encodedString, encodedString2);
+        assertEquals(expected, Base64.getEncoder().encodeToString(originalInput.getBytes()));
+    }
+
+
 }
