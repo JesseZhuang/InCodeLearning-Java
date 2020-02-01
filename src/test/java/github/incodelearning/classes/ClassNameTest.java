c60394842bbf55c4ca9aa83805c5df4adac0ecd1
Jesse Zhuang <zhzexi@gmail.com>
2020-01-31 21:18:15 -0800

    add class name test

--- src/test/java/github/incodelearning/classes/ClassNameTest.java
+++ src/test/java/github/incodelearning/classes/ClassNameTest.java
@@ -1,6 +1,11 @@
 package github.incodelearning.classes;
 
+import com.google.common.collect.ImmutableList;
 import org.junit.Test;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
@@ -38,6 +43,16 @@ public class ClassNameTest {
     }
 
     @Test
+    public void testGetCanonicalName() {
+        assertEquals("github.incodelearning.classes.ClassNameTest", new ClassNameTest().getClass().getCanonicalName());
+    }
+
+    @Test
+    public void testGetTypeName() {
+        assertEquals("github.incodelearning.classes.ClassNameTest", new ClassNameTest().getClass().getTypeName());
+    }
+
+    @Test
     public void testGetInterfaceImplementationNames() {
         Comparable c1 = new ClassNameTest().new Book();
         Comparable c2 = new ClassNameTest().new Car();
@@ -47,5 +62,21 @@ public class ClassNameTest {
         assertEquals("github.incodelearning.classes.ClassNameTest$Book", c1Class.getName());
         assertEquals("Car", c2Class.getSimpleName());
         assertEquals("github.incodelearning.classes.ClassNameTest$Car", c2Class.getName());
+    }
+
+    @Test
+    public void testDifferentTypeObjects() {
+        List<Class<?>> classes = ImmutableList.of(void.class, int.class, String.class, Runnable.class,
+                HashMap.SimpleEntry.class, new Serializable(){}.getClass());
+        for(Class c : classes) showClass(c);
+    }
+
+    private void showClass(Class<?> c) {
+        System.out.println("getName():          " + c.getName());
+        System.out.println("getCanonicalName(): " + c.getCanonicalName());
+        System.out.println("getSimpleName():    " + c.getSimpleName());
+        System.out.println("getTypeName():         " + c.getTypeName());
+        System.out.println("toString():         " + c.toString());
+        System.out.println();
     }
 }
