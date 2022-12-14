236d7b3a5ce4e9ed1f94ac2a7d950b728d730856
Zexi Jesse Zhuang <JesseZhuang@users.noreply.github.com>
2022-12-14 01:14:24 +0000

    formatting

--- src/test/java/github/incodelearning/basics/DequeTest.java
+++ src/test/java/github/incodelearning/basics/DequeTest.java
@@ -9,14 +9,13 @@ import java.util.List;
 
 import org.junit.Test;
 
-
-
 public class DequeTest {
     @Test
     public void testLinkedListAsStack() {
         Deque<Integer> stack = new LinkedList<>(); // can be declared as Deque or LinkedList
-        List<Integer> list = Arrays.asList(1,2,3);
-        for (int i: list) stack.push(i);
-        assertEquals(Arrays.asList(3,2,1), stack);
+        List<Integer> list = Arrays.asList(1, 2, 3);
+        for (int i : list)
+            stack.push(i);
+        assertEquals(Arrays.asList(3, 2, 1), stack);
     }
 }
