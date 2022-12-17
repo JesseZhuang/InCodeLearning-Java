81dffa31814fff0cde690afe1089420f5487ebd5
Jesse Zhuang <zhzexi@gmail.com>
2022-12-17 15:51:57 -0800

    negate with binary search

--- src/test/java/github/incodelearning/basics/BitTest.java
+++ src/test/java/github/incodelearning/basics/BitTest.java
@@ -1,8 +1,11 @@
 package github.incodelearning.basics;
 
-import static org.junit.Assert.assertEquals;
-
 import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import static org.junit.Assert.assertEquals;
 
 public class BitTest {
     @Test
@@ -10,5 +13,6 @@ public class BitTest {
         // negated = -original-1
         assertEquals(0xffff0000, ~0x0000ffff); // -65536, 65535
         assertEquals(0xffffffff, ~0x00000000); // -1, 0
+        assertEquals(2, ~Collections.binarySearch(Arrays.asList(1, 2, 4), 3));
     }
 }
