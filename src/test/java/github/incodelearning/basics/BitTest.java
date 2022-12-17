9f4648f0ff9ca9b47537d4a7c74f4e4cd6c8123e
Jesse Zhuang <zhzexi@gmail.com>
2022-12-17 15:59:32 -0800

    add java right shifts

--- src/test/java/github/incodelearning/basics/BitTest.java
+++ src/test/java/github/incodelearning/basics/BitTest.java
@@ -15,4 +15,12 @@ public class BitTest {
         assertEquals(0xffffffff, ~0x00000000); // -1, 0
         assertEquals(2, ~Collections.binarySearch(Arrays.asList(1, 2, 4), 3));
     }
+
+    @Test
+    public void testJavaRightShifts() {
+        assertEquals(-2, 0xfffffffe); // last byte: 1111 1110
+        assertEquals(-1, -2 >> 1); // arithmetic (signed) shift
+        assertEquals(2147483647, -2 >>> 1); // logical (unsigned) shift
+        assertEquals(2147483647, 0x7fffffff); // first byte 0111 1111
+    }
 }
